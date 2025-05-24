a36c0ea1f5772e9f86c17fd2e3b07743614ec028
tanisha2803 <156787381+tanisha2803@users.noreply.github.com>
2025-05-24 11:24:19 +0530

    server.js

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 require('dotenv').config();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
+const leadRoutes = require('./routes/leads');
+app.use('/api/leads', leadRoutes);
+
 app.get('/', (req, res) => {
-    res.send('TrackFlow CRM backend is running!!');
+    res.send('TrackFlow CRM Backend - Lead Management API');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
